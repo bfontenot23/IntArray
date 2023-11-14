ef45fe920e6464d9ecd94902bd1a12e6855a5c17
Beau Fontenot <104475813+bfontenot23@users.noreply.github.com>
2023-11-13 18:05:45 -0600

    Initial commit

--- src/Driver.java
+++ src/Driver.java
@@ -1,2 +1,23 @@
+<<<<<<< HEAD
 package PACKAGE_NAME;public class Driver {
+=======
+public class Driver {
+    public static void main(String[] args)
+    {
+        IntArray array = new IntArray(new int[]{4, 2, 32, 57, 22});
+        System.out.println(array);
+        array.sort(true);
+        System.out.println(array);
+        array.sort(false);
+        System.out.println(array);
+        array.randomize();
+        System.out.println(array);
+        array.add(53);
+        array.add(46);
+        array.add(69);
+        System.out.println(array.getSize());
+        array.sort(true);
+        System.out.println(array);
+    }
+>>>>>>> 400b23f (Initial commit)
 }
--- src/IntArray.java
+++ src/IntArray.java
@@ -1,2 +1,148 @@
+<<<<<<< HEAD
 package PACKAGE_NAME;public class IntArray {
+=======
+import java.util.ArrayList;
+import java.util.Random;
+
+public class IntArray {
+    private int size;
+    private ArrayList<Integer> arr;
+    private boolean sorted;
+
+    IntArray()
+    {
+        size = 0;
+        arr = new ArrayList<Integer>();
+        sorted = false;
+    }
+
+    IntArray(int[] arr)
+    {
+        size = arr.length;
+        this.arr = new ArrayList<Integer>();
+        for(int num: arr)
+        {
+            this.arr.add(num);
+        }
+        sorted = false;
+    }
+
+    IntArray(ArrayList<Integer> arr)
+    {
+        size = arr.size();
+        this.arr = arr;
+        sorted = false;
+    }
+
+    /*
+    getSize - returns the size of the array
+    @return - size of the array as an integer
+     */
+    public int getSize()
+    {
+        return size;
+    }
+
+    /*
+    isSorted - returns whether the array is sorted
+    @return - whether the array is sorted as an integer
+     */
+    public boolean isSorted()
+    {
+        return sorted;
+    }
+
+    public ArrayList<Integer> getArr()
+    {
+        return arr;
+    }
+
+    public void randomize()
+    {
+        ArrayList<Integer> narr = new ArrayList<Integer>();
+        Random numGen = new Random();
+        int index;
+        boolean done = false;
+        while(!done)
+        {
+            if (arr.size() != 0)
+            {
+                index = numGen.nextInt(0, arr.size());
+                narr.add(arr.get(index));
+                arr.remove(index);
+            }
+            else
+            {
+                done=true;
+            }
+        }
+        arr = narr;
+        sorted = false;
+    }
+
+    public String toString()
+    {
+        return arr.toString();
+    }
+
+    public void add(Integer num)
+    {
+        arr.add(num);
+        size++;
+    }
+
+    public void sort(boolean lth)
+    {
+        if(lth)
+        {
+            int current = 0;
+            int next = 0;
+            boolean swapped = false;
+            boolean done = false;
+
+            while(!done)
+            {
+                swapped = false;
+                for(int i = 0; i < arr.size()-1; i++)
+                {
+                    if(arr.get(i)>arr.get(i+1))
+                    {
+                        current = arr.get(i);
+                        next = arr.get(i+1);
+                        arr.set(i, next);
+                        arr.set(i+1, current);
+                        swapped = true;
+                    }
+                }
+                if(!swapped) done = true;
+            }
+        }
+        else
+        {
+            int current = 0;
+            int next = 0;
+            boolean swapped = false;
+            boolean done = false;
+
+            while(!done)
+            {
+                swapped = false;
+                for(int i = 0; i < arr.size()-1; i++)
+                {
+                    if(arr.get(i)<arr.get(i+1))
+                    {
+                        current = arr.get(i);
+                        next = arr.get(i+1);
+                        arr.set(i, next);
+                        arr.set(i+1, current);
+                        swapped = true;
+                    }
+                }
+                if(!swapped) done = true;
+            }
+        }
+        sorted=true;
+    }
+
+>>>>>>> 400b23f (Initial commit)
 }
